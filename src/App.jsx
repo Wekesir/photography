4cce9c37250e50b8a4a39fbefe825d37fd6e4728
Wekesa Kennedy <kenwekesir@gmail.com>
2024-02-03 11:59:37 +0300

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,28 +9,25 @@ import Login from '../pages/Login'
 function App() {
   const [loggedInUserData, setLoggedInUserData] = useState(null);
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={ <Root/> }>
+          <Route path="/login" element={ <Login /> }></Route>
+      </Route>
+    )
+  );
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <UserContext.Provider value={{loggedInUserData, setLoggedInUserData}}> 
+      <div> <RouterProvider router={ router }/> </div>
+    </UserContext.Provider> 
+  )
+}
+
+const Root = () => {
+  return (
+    <>     
+      <Outlet />    
     </>
   )
 }
