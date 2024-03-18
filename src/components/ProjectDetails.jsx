6e06701a89e551635d98565d390e9c71299fbc11
Wekesa Kennedy <kenwekesir@gmail.com>
2024-03-18 07:57:42 +0300

    Delete File

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -10,7 +10,7 @@ import '../../node_modules/react-toastify/dist/ReactToastify.css'
 export default function ProjectDetails() {
 
     const { id, folder } = useParams()
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const [isLoading, setIsLoading] = useState(true)
     const [files, setFiles] = useState([])
 
@@ -20,7 +20,7 @@ export default function ProjectDetails() {
         useEffect(()=>{
             axios.post(BACKEND_SERVER + "/files/get-folder-files.php", { FOLDER_ID : id })
                 .then(response => {
-                    if(response.data.status == 1){ console.log(response.data.files)
+                    if(response.data.status == 1){ 
                         setFiles(response.data.files)
                         setIsLoading(false)
                     } else {
@@ -33,8 +33,33 @@ export default function ProjectDetails() {
         }, [])
     } else {
         navigate("/projects")
+    } 
+
+    function handleDelete(event, file) { 
+        //Find the span.spinner-border and remove the d-none
+        event.target.querySelector("span.spinner-border").classList.remove("d-none")
+
+        //amke an axios post too delete-file.php
+        axios.post( BACKEND_SERVER + "/files/delete-file.php", file)
+            .then(response =>{
+
+                if(response.data.status == 1 && response.data?.status) {
+                    //Loop through all the files and remove the deleted file 
+                    let newFiles = files.filter(f => f.filename != file.filename)
+                    setFiles(newFiles)
+                    notify(response.data.msg)
+
+                } else {
+                    notify(response.data.msg)
+                }
+            })
+            .catch(error => {
+                notify("Error deleting file: " + error)
+            })
+
+        //Hide the spinner
+        event.target.querySelector("span.spinner-border").classList.add("d-none")
     }
-      
 
     function notify(message){
         toast(message)
@@ -43,7 +68,7 @@ export default function ProjectDetails() {
   return (
     <>
         <div className="container-fluid overflow-y-auto" style={{maxHeight:'84.5vh'}}>
-            <h5 className="text-white mb-3"><i className="bi bi-folder-fill"></i> &nbsp; {folder} </h5>
+            <h5 className="text-white mb-3"><i className="bi bi-folder-fill"></i> &nbsp; { folder } </h5>
 
             {isLoading ? (
                 <div className="text-center align-middle">
@@ -72,8 +97,8 @@ export default function ProjectDetails() {
                                                     <i className="bi bi-three-dots-vertical"></i>
                                                 </button>
                                                 <ul className="dropdown-menu dropdown-menu-dark">
-                                                    <li onClick={ ()=>{handleDownloadFile(file)} }><Link className="dropdown-item" to="#"><i className="bi bi-download"></i> &nbsp; Download</Link></li>
-                                                    <li><Link className="dropdown-item" to="#"><i className="bi bi-trash3"></i> &nbsp; Delete</Link></li>
+                                                    <li onClick={ (event)=>{handleDownloadFile(event, file)} }><Link className="dropdown-item" to="#"><i className="bi bi-download"></i> &nbsp; Download &nbsp; <span className="spinner-border spinner-border-sm d-none" aria-hidden="true"></span> </Link></li>
+                                                    <li onClick={ (event)=>{handleDelete(event, file)} }><Link className="dropdown-item" to="#"><i className="bi bi-trash3"></i> &nbsp; Delete &nbsp;  <span className="spinner-border spinner-border-sm d-none" aria-hidden="true"></span>  </Link></li>
                                                 </ul>
                                             </div>
                                         </div>
