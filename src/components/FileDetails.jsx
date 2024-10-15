06c85f7ab7d18fd6d42264fe36a2b8fe62f9dc59
Wekesa Kennedy <kenwekesir@gmail.com>
2024-10-16 02:07:16 +0300

    File Format

--- src/components/FileDetails.jsx
+++ src/components/FileDetails.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import { getRealFileName, getFileNameExtension } from '../utils/helpers';
+import React from "react";
+import { getRealFileName, getFileNameExtension } from "../utils/helpers";
 
-export default function FileDetails({file, project}) { 
+export default function FileDetails({ file, project }) {
   return (
     <React.Fragment>
-        <div className="text-center text-white-50" style={{fontSize:'80px'}}>
+      <div className="text-center text-white-50" style={{ fontSize: "80px" }}>
         <i className="bi bi-file-earmark-image"></i>
-        </div>
-        <table className="table table-sm table-borderless table-dark" >
+      </div>
+      <table className="table table-sm table-borderless table-dark">
         <tbody>
-            <tr>
+          <tr>
             <th scope="row">File name</th>
             <td> {getRealFileName(file.filename)} </td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <th scope="row">File type</th>
             <td> {getFileNameExtension(file.filename)} </td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <th scope="row">Size</th>
             <td>Larry</td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <th scope="row">Project</th>
             <td> {project} </td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <th scope="row">Modified</th>
             <td> {file.created_on ?? "NULL"} </td>
-            </tr>
+          </tr>
         </tbody>
-        </table>
+      </table>
     </React.Fragment>
-  )
+  );
 }
